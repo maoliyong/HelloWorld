dd71db4a0a191333b08b3c6b972fef7308360940
mly <1084654850@qq.com>
2020-08-31 23:36:19 +0800

    添加了testNG jar包和其他问题

---
+++ chapter5/src/main/java/com/coures/testng/BasicAnnotion.java
@@ -0,0 +1,11 @@
+package com.coures.testng;
+
+
+import org.testng.annotations.Test;
+
+public class BasicAnnotion {
+    @Test
+    public void testng(){
+        System.out.println("testng");
+    }
+}
